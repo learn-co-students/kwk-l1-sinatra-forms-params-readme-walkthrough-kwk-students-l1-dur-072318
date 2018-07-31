cf1d825ef516f45c627ef91fa24c784da83dd4ab
delaney washington <delaneywashington123@gmail.com>
2018-07-31 18:56:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,10 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/food_form' do
-    erb :food_form
+  get '/name_form' do
+    erb :name_form
   end
 
   # Add your post route and action below
-
+  post '/newname' do
+    @your_first_and_last_name=params[:your_first_and_last_name]
+    @celebrity_crush=params[:celebrity_crush]
+    erb :name
+  end
 end
